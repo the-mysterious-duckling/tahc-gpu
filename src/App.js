98db0cdfd96a3e21510bf6d254772aa36633d14c
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-05-23 22:02:23 +0600

    client code setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Shared/Header';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path=''></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
