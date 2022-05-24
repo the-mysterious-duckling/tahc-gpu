aa4c0c6be017404b79461f07740ace9f70a86483
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-05-24 21:31:36 +0600

    navbar and footer

---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className="flex items-center justify-center ">
+            <div className="w-24 h-24 border-l-2 border-primary rounded-full animate-spin"></div>
+        </div>
+    );
+};
+
+export default Loading;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        tahctheme: {
+          primary: "#E9D5CA",
+          secondary: "#827397",
+          accent: "#4D4C7D",
+          neutral: "#363062",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
   },
-  plugins: [],
+
+  plugins: [require("daisyui")],
 }
